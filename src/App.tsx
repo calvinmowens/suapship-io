56eccec59129453679227eafc0070007fd1fae0b
Calvin Owens <calvinowens@Calvins-MacBook-Pro.local>
2023-11-15 19:30:46 -0500

    change index.css, it was broken, add router for React

--- src/App.tsx
+++ src/App.tsx
@@ -2,34 +2,53 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { RouterProvider, createBrowserRouter, Outlet } from 'react-router-dom'
+import AllPosts from "./AllPosts";
+import PostView from "./PostView";
+import Welcome from "./Welcome";
+import MessageBoard from "./MessageBoard";
+import Navbar from './Navbar'
+
+// This tells the browser how to route users to the right component in navigation
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <MessageBoard />,
+        children: [
+          {
+            path: ":pageNumber",
+            element: <AllPosts />
+          },
+          {
+            path: "post/:postId",
+            element: <PostView />
+          },
+        ],
+      },
+      {
+        path: "welcome",
+        element: <Welcome />
+      },
+    ],
+  },
+]);
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  // This imports the router, defined above into our application, into our application
+  return <RouterProvider router={router} />
 }
 
-export default App
+export default App;
+
+function Layout() {
+  return <>
+    <Navbar />
+    <Outlet />
+  </>
+}
